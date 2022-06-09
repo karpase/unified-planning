427b345d94ff3f01ea332d4d78862c53aa933490
Erez Karpas <karpase@gmail.com>
2022-06-09 09:19:29 +0300

    original compilation implemented

--- unified_planning/transformers/robustness_verification.py
+++ unified_planning/transformers/robustness_verification.py
@@ -283,14 +283,14 @@ class RobustnessVerifier(Transformer):
                 self._new_problem.add_action(a_w)
 
             # Phantom version            
-            a_p = self.create_action_copy(action, "_p")
-            a_p.add_precondition(failure)
-            #for effect in action.effects:
-            #    if effect.value.is_true():
-            #        a_p.add_precondition(Not(self.get_waiting_version(effect.fluent)))
-            #a_p.add_precondition(waiting(action.agent.obj))             
-            #a_p.add_precondition(failure)   
-            self._new_problem.add_action(a_p)
+            # a_p = self.create_action_copy(action, "_p")
+            # a_p.add_precondition(failure)
+            # #for effect in action.effects:
+            # #    if effect.value.is_true():
+            # #        a_p.add_precondition(Not(self.get_waiting_version(effect.fluent)))
+            # #a_p.add_precondition(waiting(action.agent.obj))             
+            # #a_p.add_precondition(failure)   
+            # self._new_problem.add_action(a_p)
 
         for f in self._problem.fluents:
             params = {}
